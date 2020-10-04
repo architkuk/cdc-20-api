f51b1b0475f156aef9930e8a5155be0382fc436d
Archit Kulkarni <30812804+architkuk@users.noreply.github.com>
2020-10-04 00:47:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,28 +8,27 @@ model = lgb.Booster(model_file='model.txt')
 app = Flask(__name__)
 
 # routes
-
-
 @app.route('/', methods=['POST'])
+
 def predict():
     # get data
     data = request.get_json(force=True)
 
     # create list from data
-    model_data = []
-    model_data.append(float(data['income']))
-    model_data.append(float(data['loanAmt']))
-    model_data.append(float(data['propertyVal']))
-    model_data.append(int(data['type']))
-    model_data.append(int(data['occupiedUnits']))
-    model_data.append(int(data['purpose']))
-    model_data.append(int(data['businessOrCommercial']))
+    #model_data = []
+    #model_data.append(float(data['income']))
+    #model_data.append(float(data['loanAmt']))
+    #model_data.append(float(data['propertyVal']))
+    #model_data.append(int(data['type']))
+    #model_data.append(int(data['occupiedUnits']))
+    #model_data.append(int(data['purpose']))
+    #model_data.append(int(data['businessOrCommercial']))
 
     # predictions
     #result = model.predict([model_data])
 
     # send back to browser
-    output = {'results': int(44))}
+    output = {'results': 44)}
 
     # return data
     #return jsonify(results=output)
