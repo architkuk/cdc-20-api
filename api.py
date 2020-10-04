d40c1fa6f2ddfc455333fa63fa9f552040a22ea3
Archit Kulkarni <30812804+architkuk@users.noreply.github.com>
2020-10-03 23:38:48 -0400

    Update api.py

--- api.py
+++ api.py
@@ -8,22 +8,24 @@ model = lgb.Booster(model_file='model.txt')
 app = Flask(__name__)
 
 # routes
-@app.route('/', methods=['POST'])
 
+
+@app.route('/', methods=['POST'])
 def predict():
     # get data
     data = request.get_json(force=True)
+    print(data)
 
     # create list from data
     model_data = []
-    model_data.append(data['income'])
-    model_data.append(data['loanAmt'])
-    model_data.append(data['propertyVal'])
-    model_data.append(data['type'])
-    model_data.append(data['occupiedUnits'])
-    model_data.append(data['purpose'])
-    model_data.append(data['businessOrCommercial'])
-    
+    model_data.append(float(data['income']))
+    model_data.append(float(data['loanAmt']))
+    model_data.append(float(data['propertyVal']))
+    model_data.append(int(data['type']))
+    model_data.append(int(data['occupiedUnits']))
+    model_data.append(int(data['purpose']))
+    model_data.append(int(data['businessOrCommercial']))
+
     # predictions
     result = model.predict([model_data])
 
@@ -33,5 +35,6 @@ def predict():
     # return data
     return jsonify(results=output)
 
+
 if __name__ == '__main__':
-    app.run(port = 5000, debug=True)
+    app.run(port=5000, debug=True)
